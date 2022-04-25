5fa42ca3aa54ee5d1d8ee9913504c1c3b86af6ba
Camilo Marin <98034671+ElemCamilo@users.noreply.github.com>
2022-04-25 14:26:33 +0200

    maj 25/04/2022 14h26

--- static/js/script.js
+++ static/js/script.js
@@ -1,29 +1,13 @@
 
-
-//Ici les fonctions pour faire AJAX avec Javascript
-function loadDoc(url) {
-  //la variable ur contient l'id qui est une variable obtenue depuis la BD par Python
-  var xhttp;
-  xhttp=new XMLHttpRequest();
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo").innerHTML =
-      this.responseText;
-    }
-  };
-  xhttp.open("GET", url, true);
-  xhttp.send();
-}
-
-
 function data() 
 {
-    document.getElementById('hello').style.color = 'blue';
     var tuple_1 = document.getElementById('Tripnumber').value+",Tripnumber";
     var tuple_2 = document.getElementById('DriverID').value+",DriverID";
     var tuple_3 = document.getElementById('SystemType').value+",SystemType";
-    var to_send = "/first_map?Tripnumber="+tuple_1+"&DriverID="+tuple_2+"&SystemType="+tuple_3; 
-    loadDoc(to_send);
+    var liste = [[tuple_1],[tuple_2],[tuple_3]];
+    var liste_str = liste.toString();
+    document.getElementById('liste_data').value = liste_str;
+	document.getElementById('liste_data_f').submit();
 }
 
 
